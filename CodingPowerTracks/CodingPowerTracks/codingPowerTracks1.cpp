4abf8278b12d3b7d1d8518a558d809fcd8779b39
Nathan Montgomery <61637328+nathanmontgomery198@users.noreply.github.com>
2021-03-11 04:25:13 +0000

    added CodingPowerTracks ASCII

---
+++ CodingPowerTracks/CodingPowerTracks/codingPowerTracks1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << " _____           _ _             " << endl;
+	cout << "/  __ \\         | (_)            " << endl;
+	cout << "| /  \\/ ___   __| |_ _ __   __ _ " << endl;
+	cout << "| |    / _ \\ / _` | | '_ \\ / _` |" << endl;
+	cout << "| \\__/\\ (_) | (_| | | | | | (_| |" << endl;
+	cout << " \\____/\\___/ \\__,_|_|_| |_|\\__, |" << endl;
+	cout << "                            __/ |" << endl;
+	cout << "                           |___/ " << endl;
+	cout << "______                           " << endl;
+	cout << "| ___ \\                          " << endl;
+	cout << "| |_/ /____      _____ _ __      " << endl;
+	cout << "|  __/ _ \\ \\ /\\ / / _ \\ '__|     " << endl;
+	cout << "| | | (_) \ V  V /  __/ |        " << endl;
+	cout << "\\_|  \\___/ \\_/\\_/ \\___|_|        " << endl;
+	cout << "                                 " << endl;
+	cout << "                                 " << endl;
+	cout << " _____              _            " << endl;
+	cout << "|_   _|            | |           " << endl;
+	cout << "  | |_ __ __ _  ___| | _____     " << endl;
+	cout << "  | | '__/ _` |/ __| |/ / __|    " << endl;
+	cout << "  | | | | (_| | (__|   <\\__ \\    " << endl;
+	cout << "  \\_/_|  \\__,_|\\___|_|\\_\\___/    " << endl;
+	cout << "                                 " << endl;
+
+	return 0;
+}
